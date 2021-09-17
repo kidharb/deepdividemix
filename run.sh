69afe2fb84a944b10db935bf1bede784841d87da
Kidhar Bachan <kidhar.bachan@multichoice.co.za>
2021-09-17 21:10:48 +0200

    Reduced batch size, getting OOM

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python DeepUSPS.py train -r /notebooks/deepdividemix -s 432 --arch drn_d_22 --iter_size 1 --batch-size 20 --pretrained /notebooks/deepdividemix/Pretrained_Models/drn_pretraining/drn_d_22_cityscapes.pth
+python -m pdb DeepUSPS.py train -r /notebooks/deepdividemix -s 432 --arch drn_d_22 --iter_size 1 --batch-size 8 --pretrained /notebooks/deepdividemix/Pretrained_Models/drn_pretraining/drn_d_22_cityscapes.pth
 #python DeepUSPS.py train -r /notebooks/deepdividemix -s 432 --arch drn_d_22 --iter_size 1 --batch-size 20 --workers 2 --pretrained /notebooks/deepdividemix/Pretrained_Models/drn_pretraining/drn_d_22_cityscapes.pth --resume /notebooks/deepdividemix/Doc/Phase_II_Fusion/checkpoint_100.pth.tar
